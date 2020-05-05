498150c78bbcc265b96ab7cafaffacdaf727a50d
pj-laakkonen <petri@laakkonen.io>
2020-05-06 01:02:30 +0300

    bg

--- botConfig.py
+++ botConfig.py
@@ -4,7 +4,7 @@
 myBotName = "Keke the Friendly Robot"
 botTimeZone = "Europe/Helsinki"  ##See the list full below
 botAvatar = "/static/bot.png" ##This can either be an image in your static folder or a web url to an image
-chatBG = "/static/business_automation.jpg"  ##This can either be an image in your static folder or a web url to an image
+chatBG = "/static/rpa_manual.jpg"  ##This can either be an image in your static folder or a web url to an image
 useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
 confidenceLevel = 0.65 ##Bot confidence level - must be between 0.00 and 1, default is 0.65
 
